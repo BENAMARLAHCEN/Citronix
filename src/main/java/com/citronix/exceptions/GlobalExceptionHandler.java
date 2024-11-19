02d266e594a27778836f948a498131cd4b9b7a6a
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-11-19 16:40:38 +0100

    CT-30 <Enhance exception handling with custom global exception handler and improve formatting in existing exception classes>

--- src/main/java/com/citronix/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/citronix/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,33 @@
 package com.citronix.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(Map.of("error", ex.getBindingResult().getFieldError().getDefaultMessage()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<String> handleEntityExistsException(EntityExistsException ex) {
+        return ResponseEntity
+                .badRequest()
+                .header("error", ex.getMessage())
+                .build();
+    }
 }
