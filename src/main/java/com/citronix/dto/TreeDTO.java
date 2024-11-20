724041d070d4b2a235e51a2a34cd1852a7d69ece
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-11-20 23:11:37 +0100

    CT-14 <Refactor TreeDTO and Tree classes; update validation messages, rename methods for clarity, and add new ViewModel classes for Farm, Field, and Tree>

--- src/main/java/com/citronix/dto/TreeDTO.java
+++ src/main/java/com/citronix/dto/TreeDTO.java
@@ -11,12 +11,12 @@ import java.time.LocalDate;
 @Builder
 public class TreeDTO {
 
-    private Long id;
-
     @PastOrPresent
-    @ValidPlantingSeason
+    @ValidPlantingSeason(message = "Planting season is from March to May")
     private LocalDate plantingDate;
 
+    @NotNull
+    @Positive
     private Long fieldId;
 
     private Boolean isProductive;
