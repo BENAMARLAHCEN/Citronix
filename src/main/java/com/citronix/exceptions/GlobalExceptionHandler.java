ce0a24e8c109814622c2622e2b610d6f8b292cfc
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-11-20 09:41:43 +0100

    CT-30 <Enhance error handling by updating ErrorResponse class with additional fields and implementing a specific handler for EntityNotFoundException>

--- src/main/java/com/citronix/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/citronix/exceptions/GlobalExceptionHandler.java
@@ -6,21 +6,27 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(EntityNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         return ResponseEntity
                 .badRequest()
                 .body(Map.of("error", ex.getBindingResult().getFieldError().getDefaultMessage()));
-    }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(EntityExistsException.class)
