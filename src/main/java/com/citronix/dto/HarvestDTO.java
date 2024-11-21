cc2c458614f56bfdfcbabf6fe1e0d9fe5d01d040
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-11-21 22:53:10 +0100

    CT-5 <Add Harvest and HarvestDetail DTOs with validation and mapping methods>

--- src/main/java/com/citronix/dto/HarvestDTO.java
+++ src/main/java/com/citronix/dto/HarvestDTO.java
@@ -1,4 +1,30 @@
 package com.citronix.dto;
 
+import lombok.*;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class HarvestDTO {
+    private Long id;
+
+    @PastOrPresent
+    private LocalDate harvestDate;
+
+    @Pattern(regexp = "WINTER|SPRING|SUMMER|AUTUMN", message = "Season must be one of SPRING, SUMMER, FALL, WINTER")
+    private String season;
+
+    private Double totalQuantity;
+    private Double remainingQuantity;
+
+
+    private Long fieldId;
+
+    private List<HarvestDetailDTO> harvestDetails;
 }
