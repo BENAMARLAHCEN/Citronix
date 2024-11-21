e45476d1b2fcd0002b317abd5d2f754b9039e34c
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-11-21 23:32:32 +0100

    CT-20 <Implement Sale management functionality with CRUD operations in SaleController, SaleService, and SaleServiceImpl>

--- src/main/java/com/citronix/repositories/SaleRepository.java
+++ src/main/java/com/citronix/repositories/SaleRepository.java
@@ -1,4 +1,9 @@
 package com.citronix.repositories;
 
-public interface SaleRepository {
+import com.citronix.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
 }
--- src/main/java/com/citronix/services/SaleService.java
+++ src/main/java/com/citronix/services/SaleService.java
@@ -1,4 +1,13 @@
 package com.citronix.services;
 
-public class SaleService {
+import com.citronix.dto.SaleDTO;
+
+import java.util.List;
+
+public interface SaleService {
+    SaleDTO createSale(SaleDTO saleDTO);
+    SaleDTO updateSale(Long id, SaleDTO saleDTO);
+    SaleDTO getSaleById(Long id);
+    void deleteSale(Long id);
+    List<SaleDTO> getAllSales();
 }
